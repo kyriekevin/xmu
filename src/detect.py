b06574a2e263cf850408df5d550212c10d85c4d2
cd888888 <61482115+cd888888@users.noreply.github.com>
2021-03-29 11:10:20 +0800

    Update detect.py

--- src/detect.py
+++ src/detect.py
@@ -1,28 +1,31 @@
 import cv2 as cv
 import numpy as np
 
-
+num_in_roll = 41;  # 一行有的小方块数量
 # 仿射变换
+S_size = 205  # 变换后的二维码大小
+S_out_Size = 300  # 二维码输出大小
+
+
 def affine_transformation(points):
     ptr1 = points
-    ptr2 = np.float32([[0, 0], [205, 0], [205, 205], [0, 205]])
+    ptr2 = np.float32([[0, 0], [S_size, 0], [S_size, S_size], [0, S_size]])
     M = cv.getPerspectiveTransform(ptr1, ptr2)
-    res = cv.warpPerspective(input_img, M, (300, 300))
+    res = cv.warpPerspective(input_img, M, (S_out_Size, S_out_Size))
     return res
 
 
 def pixel2binary():
-    stance = 5
-    center_x = 2  # 每个小像素是5x5取中点值读取rgb值
-    center_y = 2
+    stance = S_size / num_in_roll  # 每一个小方块的边长#取指尽量将变长转换成整数
+    center = stance / 2  # 每个小像素是5x5取中点值读取rgb值
     pixel = [[0 for x in range(41)] for y in range(41)]
 
     # pixel=np.arange(41*41).reshape((41,41))
-    for i in range(41):  # 行读取
-        i_x = center_x + i * 5
-        for j in range(41):
-            j_y = center_y + j * 5
-            rgb = res[i_x, j_y]
+    for i in range(num_in_roll):  # 行读取
+        i_x = center + i * stance
+        for j in range(num_in_roll):
+            j_y = center + j * stance
+            rgb = res[(int)(i_x), (int)(j_y)]
             if rgb[2] > 150:  # 白色
                 pixel[i][j] = 0
             else:
@@ -32,8 +35,8 @@ def pixel2binary():
 
 def Write(pixel):
     file = open("001.txt", "w")
-    for i in range(41):
-        for j in range(41):
+    for i in range(num_in_roll):
+        for j in range(num_in_roll):
             file.write(str(pixel[i][j]) + ' ')
         file.write('\n')
     file.close()
@@ -55,6 +58,7 @@ if __name__ == "__main__":
     # 转换成二进制文件
     pixel = []
     pixel = pixel2binary();
+
     Write(pixel);
     # np.savetxt("001.txt",pixel)
 
