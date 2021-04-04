aa66128650009881b0ea12f8e404282b5d67ae0b
ikekeer <67625122+ikekeer@users.noreply.github.com>
2021-04-04 10:02:11 +0800

    Create JointFile.cpp

--- src/JointFile.cpp
+++ src/JointFile.cpp
@@ -1,42 +1,40 @@
-
 #include<iostream>
 using namespace std;
 
+
 //读取文件1.txt到文件 n.txt，写入文件result.txt（逐个字符读取）
 //第一行存帧编号
-void fileTest1(int n)
+void fileJoint(int n)
 {
-	string b = ".txt";
+	string fileNameTail = ".txt";
 	ofstream outfile("result.txt", ios::app);
-	int frame = 1;  //记录帧编号
+	int frameNo = 1;  //记录帧编号
 	for (int i = 1;i <= n;i++)
 	{
-		string a = to_string(i);
-		string c = a + b;
-		ifstream myfile(c, ios::in);
+		string fileNameNo = to_string(i);
+		string fileName = fileNameNo + fileNameTail;
+		ifstream infile(fileName, ios::in);
 		char temp;
 		string no;
-		myfile >> noskipws;  //不跳过任意的空格和换行
-		if (!myfile.is_open() || !outfile.is_open())
+		infile >> noskipws;  //不跳过任意的空格和换行
+		if (!infile.is_open() || !outfile.is_open())
 		{
 			cout << "未成功打开文件" << endl;
 			exit(0);
 		}
-		getline(myfile, no);
-		if (stoi(no) == frame)
+		getline(infile, no);  //no获取帧编号
+		if (stoi(no) == frameNo)
 		{
-			frame++;
-			while (!myfile.eof())
+			frameNo++;
+			while (!infile.eof())
 			{
-				myfile >> temp;
+				infile >> temp;
 				outfile << temp;
 			}
 		}
-		myfile.close();
+		infile.close();
 		outfile << endl;
 	}
 	outfile.close();
 	cout << "文件已写入！\n";
 }
-
-
